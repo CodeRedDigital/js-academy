1b33ce27e3661312711ceb91b8dcbfff0d24ec31
Dave Letorey <dave@code-red.uk>
2022-04-11 12:25:46 +0100

    completed w2p1 Character Count

--- Week 02/Project - 01/index.js
+++ Week 02/Project - 01/index.js
@@ -1,1 +1,6 @@
 // Project JavaScript Here
+const text = document.querySelector("#text");
+const counter = document.querySelector("#character-count");
+text.addEventListener("input", () => {
+  counter.innerText = text.value.length
+});
