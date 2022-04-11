32fd6d696341e13f915fe4cd41ec6dd48d018939
Dave Letorey <dave@code-red.uk>
2022-04-11 13:28:14 +0100

    updated the solution to take into account emojis

--- Week 02/Project - 01/index.js
+++ Week 02/Project - 01/index.js
@@ -1,6 +1,13 @@
 // Project JavaScript Here
 const text = document.querySelector("#text");
 const counter = document.querySelector("#character-count");
+const emojiCounter = document.querySelector("#emoji-count");
 text.addEventListener("input", () => {
   counter.innerText = text.value.length
+  let emojiCount = 0
+  const textArray = text.value
+  for (let letter of textArray) {
+    emojiCount ++
+  }
+  emojiCounter.innerText = emojiCount
 });
