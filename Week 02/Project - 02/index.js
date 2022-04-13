b41c4005e5d4a61ca42315da010076bfcc25b60a
Dave Letorey <dave@code-red.uk>
2022-04-13 10:09:29 +0100

    complete word & character count workshop

--- Week 02/Project - 02/index.js
+++ Week 02/Project - 02/index.js
@@ -1,1 +1,19 @@
 // Project JavaScript Here
+const text = document.querySelector("#text");
+const wordCounter = document.querySelector("#word-count");
+const counter = document.querySelector("#character-count");
+const emojiCounter = document.querySelector("#emoji-count");
+text.addEventListener("input", () => {
+  const content = text.value
+  const rawWordArray = content.split(" ")
+  console.log(rawWordArray)
+  const filteredWordArray = rawWordArray.filter(item => {return item.length > 0})
+  console.log(filteredWordArray)
+  wordCounter.innerText = filteredWordArray.length
+  counter.innerText = content.length
+  let emojiCount = 0
+  for (let letter of content) {
+    emojiCount ++
+  }
+  emojiCounter.innerText = emojiCount
+});
