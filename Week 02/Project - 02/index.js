d2d05fd7366da8eaf9c3c470b1f762b9d7f57086
Dave Letorey <dave@code-red.uk>
2022-04-14 11:09:07 +0100

    added a regex that looks for all whitespace

--- Week 02/Project - 02/index.js
+++ Week 02/Project - 02/index.js
@@ -3,13 +3,15 @@ const text = document.querySelector("#text");
 const wordCounter = document.querySelector("#word-count");
 const counter = document.querySelector("#character-count");
 const emojiCounter = document.querySelector("#emoji-count");
+const trueWordCounter = document.querySelector("#true-word-count");
 text.addEventListener("input", () => {
   const content = text.value
   const rawWordArray = content.split(" ")
-  console.log(rawWordArray)
+  const trueRawWordArray = content.split(/[\s]+/g)
   const filteredWordArray = rawWordArray.filter(item => {return item.length > 0})
-  console.log(filteredWordArray)
+  const filteredTrueWordArray = trueRawWordArray.filter(item => {return item.length > 0})
   wordCounter.innerText = filteredWordArray.length
+  trueWordCounter.innerText = filteredTrueWordArray.length
   counter.innerText = content.length
   let emojiCount = 0
   for (let letter of content) {
