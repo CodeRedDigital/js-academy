4733797ee7678c7b9982e5c1b56885313224445c
Dave Letorey <dave@code-red.uk>
2022-04-15 10:45:55 +0100

    completed workshop 3 for week 2

--- Week 02/Project - 03/index.js
+++ Week 02/Project - 03/index.js
@@ -1,1 +1,21 @@
 // Project JavaScript Here
+const text = document.querySelector("#text");
+const wordCounter = document.querySelector("#word-count");
+const counter = document.querySelector("#character-count");
+const emojiCounter = document.querySelector("#emoji-count");
+const trueWordCounter = document.querySelector("#true-word-count");
+text.addEventListener("input", () => {
+  const content = text.value
+  const rawWordArray = content.split(" ")
+  const trueRawWordArray = content.split(/[\s]+/g)
+  const filteredWordArray = rawWordArray.filter(item => {return item.length > 0})
+  const filteredTrueWordArray = trueRawWordArray.filter(item => {return item.length > 0})
+  wordCounter.innerText = filteredWordArray.length
+  trueWordCounter.innerText = filteredTrueWordArray.length
+  counter.innerText = content.length
+  let emojiCount = 0
+  for (let letter of content) {
+    emojiCount ++
+  }
+  emojiCounter.innerText = emojiCount
+});
