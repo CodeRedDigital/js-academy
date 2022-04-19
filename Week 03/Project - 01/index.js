a91ecd5cdf2463e424fe8b20f94c9dc567e1b93a
Dave Letorey <dave@code-red.uk>
2022-04-19 09:51:19 +0100

    made changes to auto load quote and error check

--- Week 03/Project - 01/index.js
+++ Week 03/Project - 01/index.js
@@ -1,1 +1,22 @@
 // Project JavaScript Here
+const ronQuote = document.querySelector("#ronism")
+const ronifyBtn = document.querySelector("#get-quote")
+const ronify = () => {
+  fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+  .then(function (response) {
+  	// The API call was successful!
+  	if (response.ok) {
+      return response.json()
+    }
+    throw response.status
+  }).then(function (data){
+    console.log(data)
+    ronQuote.textContent = data[0]
+  }).catch(function (error) {
+  	// There was an error
+  	console.warn(error);
+    ronQuote.textContent = "Sorry Ron is out of quotes, something went wrong, try again later"
+  });
+}
+ronifyBtn.addEventListener("click", ronify)
+ronify()
