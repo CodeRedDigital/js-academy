e768fe36bf935a99df6fa67b8c524b66caefc033
Dave Letorey <dave@code-red.uk>
2022-04-27 11:13:25 +0100

    completed week 4 1 & 2

--- Week 04/Project - 01/index.js
+++ Week 04/Project - 01/index.js
@@ -1,1 +1,48 @@
 // Project JavaScript Here
+const api = "https://vanillajsacademy.com/api/dragons.json";
+const app = document.querySelector("#app");
+const error = (error) => {
+  let errorHTML = null;
+  if (error === "No Posts") {
+    errorHTML = `<p>There are no articles at this time something has gone wrong, check back later.</p>`;
+  } else {
+    errorHTML = `<p>There has been an error of type <strong>${error}</strong></p>`;
+  }
+  app.classList.remove("loading");
+  app.innerHTML = errorHTML;
+};
+const buildPage = (data) => {
+  const articles = data.articles;
+  if (!articles || articles.length < 1) {
+    throw error("No Posts");
+  }
+  const listHTML = articles
+    .map((article) => {
+      return `
+    <li>
+      <a href="${article.url}">
+        <h3>${article.title}</h3>
+      </a>
+      <p class="description">${article.article}</p>
+      <footer>By: <strong>${article.author}</strong> - <time datetime="2018-07-07">${article.pubdate}</time></footer>
+    </li>
+  `;
+    })
+    .join("");
+  const heading = `<h2>${data.publication}</h2>`;
+  app.classList.remove("loading");
+  app.innerHTML = `
+    ${heading}
+    <ol class="articles">${listHTML}</ol>
+  `;
+};
+const posts = async () => {
+  const response = await fetch(api);
+  if (!response.ok) {
+    // if NOT OK throw error for the catch block to handle
+    return error(response.status);
+  }
+  const data = await response.json();
+  buildPage(data);
+};
+posts();
--- Week 04/Project - 02/index.js
+++ Week 04/Project - 02/index.js
@@ -1,1 +1,61 @@
 // Project JavaScript Here
+const postApi = "https://vanillajsacademy.com/api/dragons.json";
+const userApi = "https://vanillajsacademy.com/api/dragons-authors.json"
+const app = document.querySelector("#app");
+const error = (error) => {
+  let errorHTML = null;
+  if (error === "No Articles") {
+    errorHTML = `<p>There are no articles at this time something has gone wrong, check back later.</p>`;
+  } else {
+    errorHTML = `<p>There has been an error of type <strong>${error}</strong></p>`;
+  }
+  app.classList.remove("loading");
+  app.innerHTML = errorHTML;
+};
+const buildPage = (data) => {
+  const articles = data[0].articles;
+  const authors = data[1].authors
+  if (!articles || articles.length < 1) {
+    throw error("No Articles");
+  }
+  const listHTML = articles
+    .map((article) => {
+      let bio = authors.find(author => {
+        return author.author === article.author
+      })
+      return `
+    <li>
+      <a href="${article.url}">
+        <h3>${article.title}</h3>
+      </a>
+      <time datetime="${article.pubdate}">${article.pubdate}</time>
+      <p class="description">${article.article}</p>
+      <footer><h4>By:  ${article.author}</h4>
+      ${bio ? `<p>${bio.bio}</p>` : ""}
+      </footer>
+    </li>
+  `;
+    })
+    .join("");
+  const heading = `<h2>${data[0].publication}</h2>`;
+  app.classList.remove("loading");
+  app.innerHTML = `
+    ${heading}
+    <ol class="articles">${listHTML}</ol>
+  `;
+};
+const data = () => {
+  Promise.all([
+    fetch(postApi),
+    fetch(userApi)
+  ]).then(responses => {
+    return Promise.all(responses.map(response => {
+      return response.json()
+    }))
+  }).then(data => {
+    buildPage(data)
+  }).catch(() => {
+    error("Data not fetched")
+  })
+};
+data();
