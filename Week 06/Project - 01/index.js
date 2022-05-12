f47e46ca8af636c3419d6cfc8d086d986437e13a
Dave Letorey <dave@code-red.uk>
2022-05-12 11:39:49 +0100

    added the week 6 workshops 1 & 2

--- Week 06/Project - 01/index.js
+++ Week 06/Project - 01/index.js
@@ -1,1 +1,20 @@
 // Project JavaScript Here
+const main = document.querySelector("main")
+const inputs = document.querySelectorAll("input")
+const textarea = document.querySelector("textarea")
+main.addEventListener("input", (event) => {
+  window.localStorage.setItem(event.target.id, event.target.value)
+})
+main.addEventListener("submit", () => {
+  inputs.forEach((inp) => {
+    window.localStorage.removeItem(inp.id)
+  })
+  window.localStorage.removeItem("more")
+})
+const fillInputs = () => {
+  inputs.forEach((inp) => {
+    inp.value = window.localStorage.getItem(inp.id)
+  })
+  textarea.value = window.localStorage.getItem("more")
+}
+fillInputs()
--- Week 06/Project - 02/index.js
+++ Week 06/Project - 02/index.js
@@ -1,1 +1,45 @@
 // Project JavaScript Here
+/*!
+ * Serialize all form data into an object
+ * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {FormData} data The FormData object to serialize
+ * @return {String}        The serialized form data
+ */
+function serialize (data) {
+	let obj = {};
+	for (let [key, value] of data) {
+		if (obj[key] !== undefined) {
+			if (!Array.isArray(obj[key])) {
+				obj[key] = [obj[key]];
+			}
+			obj[key].push(value);
+		} else {
+			obj[key] = value;
+		}
+	}
+	return obj;
+}
+
+
+const form = document.querySelector("#save-me")
+form.addEventListener("input", (event) => {
+  const data = new FormData(form)
+  localStorage.setItem("formData", JSON.stringify(serialize(data)))
+  console.log(JSON.stringify(serialize(data)))
+})
+form.addEventListener("submit", () => {
+  window.localStorage.removeItem("formData")
+})
+const fillInputs = () => {
+  const data = JSON.parse(localStorage.getItem("formData"))
+  const elements = form.elements
+  for (i = 0; i < elements.length; i++) {
+    if (elements[i].name) {
+      const key = elements[i].name
+      if (data[key]) {
+        elements[i].value = data[key]
+      }
+    }
+  }
+}
+fillInputs()
